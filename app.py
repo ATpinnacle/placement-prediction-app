de8ebb1a6f8953084ecaff07da92ff1eb468321d
Aditya Tiwari <adityatiwari_23ep009@dtu.ac.in>
2024-12-15 10:13:22 +0530

    Fix validation logic

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ st.title("Placement Prediction")
 cgpa = st.number_input("Enter CGPA:", min_value=0.0, max_value=10.0, value=6.0)
 iq = st.number_input("Enter IQ:", min_value=50, max_value=200, value=100)
 
-input_data = [[cgpa, iq]]
-input_data_scaled = scaler.transform(input_data)  
-
 if st.button('Predict'):
-
-    prediction = model.predict(input_data_scaled)
-    if prediction == 1:
-        st.write("You are likely to be placed!")
+    if iq > 200 or iq < 50 or cgpa > 10.0 or cgpa < 0.0:
+        st.error("Please provide valid inputs within the specified range!")
     else:
-        st.write("You are less likely to be placed.")
+        input_data = [[cgpa, iq]]
+        prediction = model.predict(input_data)
+        if prediction == 1:
+            st.write("You are likely to be placed!")
+        else:
+            st.write("You are less likely to be placed.")
+
